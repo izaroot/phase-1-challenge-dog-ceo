a50ed94c9c919fb17db9fb87c6d062c17fcce2af
jennafritz <jennafritz24@gmail.com>
2021-04-12 14:34:04 -0400

    Completed challenge 3, working on Challenge 4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ console.log('%c HI', 'color: firebrick')
 document.addEventListener("DOMContentLoaded", function(){
     let dogImages = document.querySelector("#dog-image-container")
     let dogBreeds = document.querySelector("#dog-breeds")
-
+    let breedDropdown = document.querySelector("#breed-dropdown")
     fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(resp => resp.json())
     .then((act) => {
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function(){
             blankImg.src = element
             dogImages.append(blankImg)
         });
-        console.log(dogArray[0])
     })
 
 
@@ -24,11 +23,25 @@ document.addEventListener("DOMContentLoaded", function(){
             let blankLi = document.createElement("li")
             blankLi.innerText = element
             dogBreeds.append(blankLi)
-            console.log(breedArray)
+            blankLi.addEventListener('click', function(){
+                blankLi.style.color = "red"
+            breedDropdown.addEventListener('change', function(evt) {
+                let breedName = blankLi.innerText
+                console.log(breedName)
+                let firstLetter = breedName.substring(0,1)
+                console.log(firstLetter)
+                let dropdownLetter = evt.target.value
+                if (firstLetter !== `${dropdownLetter}`) {
+                    return blankLi.style.display = "none"
+                } else if (firstLetter === `${dropdownLetter}`){
+                    return blankLi.style.display = "block"
+                }
+            // })    
+            // })
         });
         // console.log(dogArray[0])
     })
-
+})
 
 })
-// console.log(dogImages)
+})
